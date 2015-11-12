129e10fa7704f6f88e0be33011ddc1d020b5a194
redpois0n <red@cmail.nu>
2015-11-12 20:00:26 +0100

    Basic redirection test, redirects one domain to another

---
+++ background.js
@@ -0,0 +1,13 @@
+var host = "https://destination.com";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://origin.com/*",
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
