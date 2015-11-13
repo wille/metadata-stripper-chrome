420a5db65cc744ef1e176e7513fe958dc9b3b859
redpois0n <red@cmail.nu>
2015-11-13 19:36:19 +0100

    facebook removal of fref=ts working

--- background.js
+++ background.js
@@ -5,18 +5,40 @@ chrome.webRequest.onBeforeRequest.addListener(
 		// GET query string
 		var path = d[1];
 		
-		if (path.slice(0, 1) == "/") {
-			path = path.slice(1, path.length);
-		}
 		
 		// protocol+hostname
 		var url = details.url.slice(0, details.url.length - d[1].length);
 		
-		if (url === "https://www.facebook.com") {
-			// Loop through GET parameters and remove fref etc...
+		if (path.slice(0, 1) == "/" && url.slice(url.length - 1, url.length) == "/") {
+			path = path.slice(1, path.length);
 		}
 		
-        return {redirectUrl: url + path};
+		if (url === "https://www.facebook.com" && path.indexOf("?") != -1) {
+			// Loop through GET parameters and remove fref etc...
+			
+			// Replace ? with & to not confuse split()
+			var params = path.slice(path.indexOf("?")).replace("?", "&").split("&");
+			
+			
+			var redirect = false;
+			
+			var newpath = path.slice(0, path.indexOf("?") + 1);
+			for (var i = 0; i < params.length; i++) {
+				if (params[i] == "fref=ts") {
+					redirect = true;
+				} else {
+					newpath += params[i];
+				}
+			}
+			
+			if (redirect) {
+				return {redirectUrl: url + newpath };
+			}
+		}
+		
+		
+		// Return empty response, no action
+        return { };
     },
     {
         urls: [
