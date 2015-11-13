628897afb767b696182a0c16f484c171cd6dd7f7
redpois0n <red@cmail.nu>
2015-11-13 18:57:31 +0100

    Facebook rule

--- background.js
+++ background.js
@@ -1,18 +1,26 @@
-var host = "https://destination.com/";
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
 		var d = details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/);
+		
+		// GET query string
 		var path = d[1];
 		
 		if (path.slice(0, 1) == "/") {
 			path = path.slice(1, path.length);
 		}
 		
-        return {redirectUrl: host + path};
+		// protocol+hostname
+		var url = details.url.slice(0, details.url.length - d[1].length);
+		
+		if (url === "https://www.facebook.com") {
+			// Loop through GET parameters and remove fref etc...
+		}
+		
+        return {redirectUrl: url + path};
     },
     {
         urls: [
-            "*://origin.com/*",
+            "*://www.facebook.com/*",
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
